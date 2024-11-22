58e031ea9ee935de8bfc4270b7de43ea45090eff
Pedro Silveira <pedrohpss@gmail.com>
2024-11-22 21:50:11 +0000

    Elysia Cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
+import { note } from "./controllers/notes";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app = new Elysia()
+  .use(cors())
+  .use(note)
+  .get("/", () => "Hello Elysia")
+  .get('/bot-stats', () => {
+    console.log("Test, start");
+    return { onlineUsers: Math.floor(Math.random() * 110000), activeBots: Math.floor(Math.random() * 11) };
+  })
+.listen(3001);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
